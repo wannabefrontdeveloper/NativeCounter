881e2b65444a670d680b767dcf39e2f0b4d0d8eb
wannabefrontdeveloper <giwonk@naver.com>
2024-03-05 11:47:27 +0900

    코드추가

---
+++ android/app/src/main/java/com/nativecounter/CounterView.kt
@@ -0,0 +1,37 @@
+package com.nativecounter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.uimanager.events.RCTEventEmitter
+import com.nativecounter.databinding.CounterViewBinding
+import com.facebook.react.uimanager.events.RCTModernEventEmitter
+
+class CounterView(val context: ReactContext): FrameLayout(context) {
+    private val binding: CounterViewBinding
+
+    fun setLeftButtonText(text: String) {
+        binding.button.text=text
+    }
+    fun setRightButtonText(text: String) {
+        binding.button2.text=text
+    }
+    fun setValue(value:Int) {
+        binding.textView.text=value.toString()
+    }
+    fun setupEvents() {
+        val eventEmitter = context.getJSModule(RCTEventEmitter:: class.java)
+        binding.button.setOnClickListener {
+            eventEmitter.receiveEvent(id, "pressLeftButton", null)
+        }
+        binding.button2.setOnClickListener {
+            eventEmitter.receiveEvent(id, "pressRightButton", null)
+        }
+    }
+    init {
+       val inflater = LayoutInflater.from(context)
+        binding = CounterViewBinding.inflate(inflater, this, true)
+        this.setupEvents()
+    }
+}
